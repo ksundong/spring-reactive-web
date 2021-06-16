34fc0ddc1ecf7744ad8b6de61bd24c6d2d4343d1
Dion <ksundong@gmail.com>
2021-06-16 23:51:51 +0900

    feat: 판매상품 데이터를 다룰 POJO 선언

---
+++ demohacking-spring-boot-ch2-reactive/src/main/java/dev/idion/hackingspringboot/reactive/domain/Item.java
@@ -0,0 +1,56 @@
+package dev.idion.hackingspringboot.reactive.domain;
+
+import java.util.Objects;
+import org.springframework.data.annotation.Id;
+
+public class Item {
+
+  private @Id String id;
+  private String name;
+  private double price;
+
+  private Item() {}
+
+  Item(String name, double price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Item item = (Item) o;
+    return Double.compare(item.getPrice(), getPrice()) == 0 && Objects
+        .equals(getName(), item.getName());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getName(), getPrice());
+  }
+}
