ea897cd6f4d991d43211c6eade9f5d0ef7be8d8f
Dion <ksundong@gmail.com>
2021-06-19 23:43:48 +0900

    feat: 장바구니에 상품담기 기능 구현

--- demohacking-spring-boot-ch2-reactive/src/main/java/dev/idion/hackingspringboot/reactive/HomeController.java
+++ demohacking-spring-boot-ch2-reactive/src/main/java/dev/idion/hackingspringboot/reactive/HomeController.java
@@ -2,9 +2,12 @@ package dev.idion.hackingspringboot.reactive;
 
 import dev.idion.hackingspringboot.reactive.domain.cart.Cart;
 import dev.idion.hackingspringboot.reactive.domain.cart.repository.CartRepository;
+import dev.idion.hackingspringboot.reactive.domain.cartitem.CartItem;
 import dev.idion.hackingspringboot.reactive.domain.item.repository.ItemRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
@@ -28,5 +31,27 @@ public class HomeController {
             .modelAttribute("cart", this.cartRepository.findById(CART_ID).defaultIfEmpty(new Cart(CART_ID)))
             .build()
     );
+  }
+
+  @PostMapping("/add/{id}")
+  public Mono<String> addToCart(@PathVariable String id) {
+    return this.cartRepository.findById(CART_ID)
+        .defaultIfEmpty(new Cart(CART_ID))
+        .flatMap(cart -> cart.getCartItems().stream()
+            .filter(cartItem -> cartItem.getItem()
+                .getId().equals(id))
+            .findAny()
+            .map(cartItem -> {
+              cartItem.increment();
+              return Mono.just(cart);
+            })
+            .orElseGet(() -> this.itemRepository.findById(id)
+                .map(CartItem::new)
+                .map(cartItem -> {
+                  cart.getCartItems().add(cartItem);
+                  return cart;
+                }))
+        ).flatMap(this.cartRepository::save)
+        .thenReturn("redirect:/");
   }
 }
--- demohacking-spring-boot-ch2-reactive/src/main/java/dev/idion/hackingspringboot/reactive/domain/cartitem/CartItem.java
+++ demohacking-spring-boot-ch2-reactive/src/main/java/dev/idion/hackingspringboot/reactive/domain/cartitem/CartItem.java
@@ -31,6 +31,10 @@ public class CartItem {
     this.quantity = quantity;
   }
 
+  public void increment() {
+    quantity += 1;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) {
