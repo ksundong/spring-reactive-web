7039b434225bc31d3dae35d3b20856e83293af4d
Dion <ksundong@gmail.com>
2021-06-20 10:42:04 +0900

    feat: 평문형 API를 사용한 검색기능 구현

--- demohacking-spring-boot-ch2-reactive/src/main/java/dev/idion/hackingspringboot/reactive/InventoryService.java
+++ demohacking-spring-boot-ch2-reactive/src/main/java/dev/idion/hackingspringboot/reactive/InventoryService.java
@@ -1,10 +1,14 @@
 package dev.idion.hackingspringboot.reactive;
 
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
 import dev.idion.hackingspringboot.reactive.domain.item.Item;
 import dev.idion.hackingspringboot.reactive.domain.item.repository.ItemRepository;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.ExampleMatcher.StringMatcher;
+import org.springframework.data.mongodb.core.ReactiveFluentMongoOperations;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -12,9 +16,11 @@ import reactor.core.publisher.Flux;
 public class InventoryService {
 
   private final ItemRepository repository;
+  private final ReactiveFluentMongoOperations fluentMongoOperations;
 
-  public InventoryService(ItemRepository repository) {
+  public InventoryService(ItemRepository repository, ReactiveFluentMongoOperations fluentMongoOperations) {
     this.repository = repository;
+    this.fluentMongoOperations = fluentMongoOperations;
   }
 
   public Flux<Item> searchByExample(String name, String description, boolean useAnd) {
@@ -30,5 +36,11 @@ public class InventoryService {
     Example<Item> probe = Example.of(item, matcher);
 
     return repository.findAll(probe);
+  }
+
+  public Flux<Item> searchByFluentExample(String name, String description) {
+    return fluentMongoOperations.query(Item.class)
+        .matching(query(where("TV tray").is(name).and("Smurf").is(description)))
+        .all();
   }
 }
