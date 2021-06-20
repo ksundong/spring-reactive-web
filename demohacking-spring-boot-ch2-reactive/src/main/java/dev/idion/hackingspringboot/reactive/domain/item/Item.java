ad755349ac3a485e31a24a6d9937cc7ac4442f3c
Dion <ksundong@gmail.com>
2021-06-20 10:19:20 +0900

    feat: 복잡한 검색을 위해서 추가적인 필드 정의

--- demohacking-spring-boot-ch2-reactive/src/main/java/dev/idion/hackingspringboot/reactive/domain/item/Item.java
+++ demohacking-spring-boot-ch2-reactive/src/main/java/dev/idion/hackingspringboot/reactive/domain/item/Item.java
@@ -1,13 +1,20 @@
 package dev.idion.hackingspringboot.reactive.domain.item;
 
+import java.time.LocalDate;
 import java.util.Objects;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.geo.Point;
 
 public class Item {
 
   private @Id String id;
   private String name;
+  private String description;
   private double price;
+  private String distributorRegion;
+  private LocalDate releaseDate;
+  private int availableUnits;
+  private Point location;
 
   private Item() {}
 
@@ -28,12 +35,52 @@ public class Item {
     this.name = name;
   }
 
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
   public double getPrice() {
     return price;
   }
 
   public void setPrice(double price) {
     this.price = price;
+  }
+
+  public String getDistributorRegion() {
+    return distributorRegion;
+  }
+
+  public void setDistributorRegion(String distributorRegion) {
+    this.distributorRegion = distributorRegion;
+  }
+
+  public LocalDate getReleaseDate() {
+    return releaseDate;
+  }
+
+  public void setReleaseDate(LocalDate releaseDate) {
+    this.releaseDate = releaseDate;
+  }
+
+  public int getAvailableUnits() {
+    return availableUnits;
+  }
+
+  public void setAvailableUnits(int availableUnits) {
+    this.availableUnits = availableUnits;
+  }
+
+  public Point getLocation() {
+    return location;
+  }
+
+  public void setLocation(Point location) {
+    this.location = location;
   }
 
   @Override
