77a285e37bf5f3ce8238ceaa0c97c55945b0ed5e
Dion <ksundong@gmail.com>
2021-06-20 19:04:47 +0900

    feat: BlockHound를 통해서 Blocking 코드 검출설정 및 예외적인 경우 허용 설정 추가

--- demohacking-spring-boot-ch2-reactive/src/main/java/dev/idion/hackingspringboot/reactive/Application.java
+++ demohacking-spring-boot-ch2-reactive/src/main/java/dev/idion/hackingspringboot/reactive/Application.java
@@ -2,11 +2,18 @@ package dev.idion.hackingspringboot.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.result.view.ViewResolutionResultHandler;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
+    BlockHound.builder()
+        .allowBlockingCallsInside(
+            ViewResolutionResultHandler.class.getCanonicalName(), "render")
+        .install();
+
     SpringApplication.run(Application.class, args);
   }
 
